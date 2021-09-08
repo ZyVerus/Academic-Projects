bba5b4ad5ca33312a92b40ac9c6270c281f11941
hugombu94 <hugombu94@gmail.com>
2021-09-07 23:31:56 -0400

    Update states_information.py
    Finished Menu Option 4 function update_population() with two nested functions.
    
    Cleaned program layout to acquire a pylint score of 10/10.

--- Python/Secure Development/State Information Application/states_information.py
+++ Python/Secure Development/State Information Application/states_information.py
@@ -9,15 +9,16 @@ Third party library utilized using a separate Anaconda environment in Visual Stu
 
 Third party libraries:
 matplotlib
+PIL (Python Imaging Library)
 
 """
 
 import os
 import sys
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import states_data as states
+import matplotlib.ticker as mticker
 from PIL import Image
+import states_data as states
 
 # Algorithm Termination Value
 SENTINEL = "0"
@@ -33,21 +34,19 @@ def main():
         # Switch
         # Display all States
         if inp == '1':
-            specific = False
             for i in states.states:
-                show_state(i, specific)
+                show_state(i, False)
         # Search for a state
         elif inp == '2':
-            specific = True
-            state_bool = get_state()
-            if state_bool:
-                show_state(state_bool, specific)
+            state = get_state()
+            if state:
+                show_state(state, True)
         # Display Top 5 Populated States
         elif inp == '3':
             show_top_population()
         # Update a state's population
         elif inp == '4':
-            print("ALERT: Function Under Construction.\nPlease check back at a later time.")
+            update_population()
         # Exit Program
         elif inp == '0':
             menu("exit")
@@ -57,26 +56,30 @@ def main():
 
 def show_state(state, specific):
     """ Show State Information """
+    # Set population value to integer
+    pop_value = int(states.states[state]["Population"])
+    print("State:\t\t", state)
+    print("Capital:\t", states.states[state]["Capital"])
+    print("Flower:\t\t", states.states[state]["Flower"])
+    print("Population:\t {:,}".format(pop_value))
     menu("sl")
-    print("State:\t", state)
-    print("Capital:", states.states[state]["Capital"])
-    print("Flower:\t", states.states[state]["Flower"])
 
     # Show Flower Picture if a specific State was selected
-    if specific == True:
+    if specific:
         # / for filepath used for interoperability between different Operating Systems.
         image_path = os.path.join(sys.path[0] + "/Flowers/" + state + ".jpg")
-        print("\nALERT:\nOpening File: " + image_path + " ...")
+        print("ALERT:\nOpening File: " + image_path + " ...\n")
         image = Image.open(image_path)
         image.show()
 
 def get_state():
     """ Requests user to specify a state they would like to display """
     state = ""
-    print("Please enter the State you would like to retrieve information for.")
     menu("back")
+    print("Please enter a State:")
     while not state:
         inp = input("\n> ").strip()
+        menu("sl")
         # Check for Sentinel Value
         if inp == SENTINEL:
             break
@@ -84,10 +87,11 @@ def get_state():
         fix_inp = ""
         # Check to see if input only contains alphabetical characters (ignores spaces)
         if inp.replace(" ", "").isalpha():
-            # Split input string value into separate a list (split() using whitespace as the default separator)
-            list = inp.split()
-            # Check each word 
-            for i in list:
+            # Split input string value into separate a list
+            # split() uses whitespace as the default separator
+            word_list = inp.split()
+            # Check each word
+            for i in word_list:
                 # Capitalizes the first letter in each word
                 fix_inp += i.capitalize() + " "
             # Strip the fixed input of any spaces at the end of the string
@@ -96,7 +100,7 @@ def get_state():
         if fix_inp in states.states.keys():
             state = fix_inp
         else:
-            print("Please enter a valid State.")
+            print("ERROR: Please enter a valid State.")
 
     return state
 
@@ -104,27 +108,32 @@ def show_top_population():
     """ Shows the states with the highest population in descending order """
     # Get state populations listed in descending order
     populations = sorted(
-        states.states.keys(),key=lambda key_:int(
+        states.states.keys(), key=lambda key_: int(
             states.states[key_]["Population"]
-            ),reverse=True
+            ), reverse=True
         )
 
     # Keep only first five top items
     populations = populations[:5]
     pop_list = []
-    print("Top 5 Populated States:")
+    print("Top 5 Populated States (Descending Order):\n")
     # Output List
     for i in populations:
         pop_value = int(states.states[i]["Population"])
-        print(i + ": {:,}".format(pop_value))
+        print(i + ":  {:,}".format(pop_value))
         pop_list.append(pop_value)
 
+    menu("sl")
     # Plot Data
     plt.bar(populations, pop_list,)
     # Get Y Values in the Bar Graph
     y_values = plt.gca().get_yticks()
-    # Format Y Values (Labels) to show Commas for Thousandths place
-    plt.gca().set_yticklabels(["{:,.0f}".format(i) for i in y_values])
+    # Set Labels Format to show Commas for Thousandths place
+    label_format = "{:,.0f}"
+    # Set FixedLocator to suppress FixedFormatter UserWarning
+    plt.gca().yaxis.set_major_locator(mticker.FixedLocator(y_values))
+    # Set Y Values (Labels)
+    plt.gca().set_yticklabels([label_format.format(i) for i in y_values])
     # Create X Label
     plt.xlabel("States", fontsize=12)
     # Create Bar Title
@@ -132,20 +141,69 @@ def show_top_population():
     # Display Bar Graph to User
     plt.show()
 
+def update_population():
+    """ Function to update state population based on User's input """
+    # Nested Function
+    def inp_population():
+        """ Validate that user input is an integer """
+        value = ""
+        while not value:
+            print("Please enter the new population value:")
+            inp = input("\n> ").strip()
+            menu("sl")
+
+            # Check for Sentinel Value
+            if inp == SENTINEL:
+                break
+            # Input Validation
+            if int_val(inp):
+                if int(inp) > 0:
+                    value = inp
+                else:
+                    print("ERROR: Population can not be 0 or a negative value.\n")
+            else:
+                print("ERROR: Population must be a number with a value of 1 or greater\n")
+        return value
+
+    # Nested Function
+    def set_population(state, population):
+        """ Set the value of the data in states_data.py dictonary """
+
+        states.states[state]["Population"] = population
+        print("Population of " + state + " set to " + population + "\n")
+        #else:
+            #print("ERROR: State could not be found.")
+
+    # Prompt for state input and run nested functions
+    #print("Please enter the State you would like to update.")
+    #menu("back")
+    state = get_state()
+    if state:
+        value = inp_population()
+        if value:
+            set_population(state, value)
+
 def menu(option):
     """ used for displaying the menu in the CLI """
-    intro = "Welcome to the Python State Capital and Flower List Application."
-    closing = ("\nThank you for using the State Capital and Flower List Application."
-               + "\nPress ENTER to close this console."
-               )
-    prompts = "\nPlease choose an option below:" \
-        + "\n1 - Display all States in alphabetical order with their respective Capital, Population, and Flower." \
-        + "\n2 - Search for a specific state and display its respective Capital, Population, and Flower (with image)." \
-        + "\n3 - Display a bar graph of the 5 most populated states and their overall population." \
+    intro = "Welcome to the Python State Capital and Flower List Application.\n"
+    closing = (
+        "Thank you for using the State Capital and Flower List Application."
+        + "\nPress ENTER to close this console."
+        )
+    prompts = (
+        "Please choose an option below:" \
+        + "\n1 - Display all States in alphabetical order with their respective" \
+        + " Capital, Population, and Flower." \
+        + "\n2 - Search for a specific state and display its respective Capital," \
+        + " Population, and Flower (with image)." \
+        + "\n3 - Display a bar graph of the 5 most populated states and their" \
+        + " overall population." \
         + "\n4 - Update the overall state population for a specific state." \
         + "\n0 - Exit Application."
-    invalid = "Invalid Selection. Please select from 1 - 4 of the menu options, or '0' to exit."
-    back = "Input '0' at any point to return to the main menu."
+        )
+    invalid = "ERROR: Invalid Selection. Please select from 1 - 4 of the menu" \
+        + " options, or '0' to exit.\n"
+    back = "BACK: Input '0' at any point to return to the main menu."
 
     if option == "init_run":
         print(intro)
@@ -158,12 +216,12 @@ def menu(option):
     elif option == "proceed":
         input("Press ENTER to proceed...")
     elif option == "sl":
-        print("\n")
+        print("")
     elif option == "exit":
         print(closing)
 
-# Interger Input Validation
-def int_check(inp):
+# Integer Input Validation
+def int_val(inp):
     """ Input validation to test for integer value """
     is_int = False
     try:
